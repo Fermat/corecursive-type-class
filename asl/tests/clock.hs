0dd6dfc6fb8b5b76b9bfc4cb3e910c005f264ec9
Fermat <fermat1217@gmail.com>
2015-09-06 16:51:22 +0000

    still untypable without explicitly manipulating equations.

--- asl/tests/clock.hs
+++ asl/tests/clock.hs
@@ -12,8 +12,11 @@ k2 :: D (S m) Z -> D Z m
 k2 = undefined
 
 f :: (forall n. D n (S m) -> D (S (Add m n)) Z) -> D Z m
-f = \ g -> k2 (g (f (\ x -> g (k1 x))))
+f g = k2 (g (f (\ x -> g (k1 x))))
 
+p :: D Z Z
+p =  f (\ x -> k1 x)
 type family Add m n
-type instance Add Z n  =  n
-type instance Add (S n) m = Add (S m) n
+type instance Add n Z  =  n
+type instance Add m (S n) = Add (S m) n
+
